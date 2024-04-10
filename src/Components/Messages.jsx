3c5ffca2682aa8dfb915261f8b76639701a445e2
Ben Shuan <benshuan@gmail.com>
2024-04-10 22:42:42 +0300

    fix

--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -37,7 +37,7 @@ export default function Messages({chatId}) {
     <div style={{ width: '100%',height:'70vh',  backgroundColor: theme.palette.background.paper, display: 'flex', flexDirection: 'column', overflow: 'auto', gap: 10 }}>
       {renderList()}
       <dialog open={imgToShow.show} style={{position:'absolute',top:10, padding: 0, margin: 'auto', borderRadius: 20,width:'100%',maxWidth:'70vw', background: 'none', maxHeight:'90vh' }}>
-        <img src={imgToShow.message} style={{ width: '100%', borderRadius: 'inherit', display: 'block', width:'100%'}} />
+        <img src={imgToShow.message} style={{ width: '100%', borderRadius: 'inherit', display: 'block'}} />
 
         <button style={{ position: 'absolute', top: 10, left: 10 }} onClick={() => setImgToShow({ show: false })}>X</button>
       </dialog>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -64,9 +64,7 @@ const router = createHashRouter ([
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     
     <RouterProvider router={router} />
     
-  </React.StrictMode>,
 )
